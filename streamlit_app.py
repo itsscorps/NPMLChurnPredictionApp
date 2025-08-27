7cc38674234902b08e7959ae1c6e69808f56cdf3
Nehemiah Pinia <pinianehemiah6@gmail.com>
2025-08-27 12:53:47 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,16 @@ st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
  st.write('**Raw data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/cleaned_dataset.csv')
-  df
+ df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/cleaned_dataset.csv')
+ df
 
  st.write('**X**')
-  X_raw = df.drop('species', axis=1)
-  X_raw
+ X_raw = df.drop('species', axis=1)
+ X_raw
 
 st.write('**y**')
-  y_raw = df.species
-  y_raw
+y_raw = df.species
+y_raw
 
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
