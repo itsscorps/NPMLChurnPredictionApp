067999b4335061b6154a987c6fbbc17b5f40fc83
Nehemiah Pinia <pinianehemiah6@gmail.com>
2025-08-27 12:52:26 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import GradientBoostongClassifier
 
 st.title('Bank Customer Churn Prediction APP')
 st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
-  st.write('**Raw data**')
+ st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/cleaned_dataset.csv')
   df
 
