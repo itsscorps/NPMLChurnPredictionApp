f252fe5cdf6a1d898436095568d09f4297db9ce0
Nehemiah Pinia <pinianehemiah6@gmail.com>
2025-08-27 13:35:39 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,11 @@ with st.expander('Data'):
  df
 
  st.write('**X**')
- X_raw = df.drop('exited', axis=1)
+ X_raw = df.drop('Exited', axis=1)
  X_raw
 
  st.write('**y**')
- y_raw = df.exited
+ y_raw = df.Exited
  y_raw
 
 
