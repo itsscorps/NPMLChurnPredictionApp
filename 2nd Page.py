5a9a15e009b1fd21cd42913b6ea09df6a29a7943
Nehemiah Pinia <pinianehemiah6@gmail.com>
2025-10-09 00:02:30 +1000

    Update 2nd Page.py

--- 2nd Page.py
+++ 2nd Page.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import joblib
+import altair as alt
+from sklearn.ensemble import GradientBoostingClassifier
 
+
+# APP TITLE AND DESCRIPTION
+
+st.title('Page TWO')
+st.info('This page predicts whether a group of customer will churn using a trained Gradient Boosting model.')
